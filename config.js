9d962cf59e258ab53a953b652c259bac76fad894
Sirenascript <limulesirena@gmail.com>
2024-12-06 00:08:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vajrhmz96H4I
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vajrhmz96H4IsEjh4a41";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/TAGPcMC.jpeg,https://files.catbox.moe/59iafh.jpg,https://files.catbox.moe/jynitk.jpg,https://files.catbox.moe/dohubs.jpg,https://files.catbox.moe/nm85c3.jpg";
 global.devs = "https://wa.me/242067274660 , https://wa.me/243849962848";
-global.sudo = process.env.SUDO || "242067274660";
-global.owner = process.env.OWNER_NUMBER || "PUT YOUR OWNER NUMBER";
+global.sudo = process.env.SUDO || "22399369990";
+global.owner = process.env.OWNER_NUMBER || "22392291649";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "true";
@@ -43,15 +43,15 @@ global.scan = "https://unknown-md-sessions-generator-1.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "PUT YOUR SESSION HERE"
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQU04aUNJa3lTcno0bDM3bmp6T2pRdVV4TVc5WDhxRXV3akd6ZTlURjUyMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiczN1VW9MYVdyOXpSdVRpOW81K2lrYkVqZkMwTG9rU21yTjhCMEUzVExpMD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFSGNmWTJMcHpSN3BXTmIzMUxIbks1ZzlkWnp6WVZPY1pqUUV0d3J5aEZZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI1ZkJONkh2ODJNaHZjc2NQSUFCaG9vY1hyZU5WWDM2NmZvcW9ad1JNV0NRPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdFQjhsYUlYMnBWMnVtS2VIenBPcmxlL2dBb05lcFYvSE16NkI2Nnh2MEk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlVlVU85N2N3MlJ1cW9jQmszamF6cTNiQ0dIMkNCR3c2SjNSdEZwZGNwQ3M9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUswZHAvNkh5QUVTWk4xcXpOSFpERHlZK21FY2NEdzJHRm1GeDJIN0FFdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMzRIcys1cS9PczBYWW12Sllld2E0OVJZWkFWelpyaXhqM0d6aUd2NGZuYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InhJNWVHZDlIR0JoMnZ1d0w2WEo2RGExU0sxcUtCenZDY2VKSzNFQ09aL2lRdGUxZmZBK08wcnNCenBDdlJENHU2cFp5M05TakFQc1FEb3FlZDlDZ0JBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NDcsImFkdlNlY3JldEtleSI6ImYvVitLMCsrbVcvUDJJQ3Q0Z3NvN0Y1MTdMMUd1U3NCMnZJbWtReVRDTVU9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6Ii1TNTBFOUdEUUgtTTA2cjQxam1qb3ciLCJwaG9uZUlkIjoiM2FkOTdkM2QtM2VmMy00MTA5LThjMWItOGFjMDg2ZjUwYjBhIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9MeUs1M3hoUGdIRmp2TEJONEdVekhUQU1oQT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIxaFNhN3lmU1IzTTZuL2xtVEhSNElYRTZoOTQ9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiRjJTREZTWVAiLCJtZSI6eyJpZCI6IjIyMzkyMjkxNjQ5OjMxQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNOUDk5OE1DRUluOXlMb0dHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiIyVW16bDJzVVFWZVhkTGpuS1EvdXkzVDVQMVRtdUhmVEZndVJYdjFiaWdjPSIsImFjY291bnRTaWduYXR1cmUiOiIyQkEvSmVCSzA4bWVqZ1JrZlVtYUlQTmk0S2J2UU9SOFdMUlVVY2w0ckZsTnhkRTR1Vy9PWTU4b1VtdnpPTkNEeVpCWFEyTHoycHBVNE45cHRhVkhCUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiNjV0aERwQzlzOVJMeHY4ZmRyUEwxWXQ4cFRGeGlQclBXMGRVSUIvUU11bkltTWtzdVBreW9wM1B2YXB3eHZCVG5uekY1WVdFRnVxYVN1UitaMkhmREE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMjM5MjI5MTY0OTozMUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJkbEpzNWRyRkVGWGwzUzQ1eWtQN3N0MCtUOVU1cmgzMHhZTGtWNzlXNG9IIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzMzNDQzMjIzfQ=="
 module.exports = {
   menu: process.env.MENU || "1",
-  HANDLERS: process.env.PREFIX || ".",
+  HANDLERS: process.env.PREFIX || "!",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`BRAND PRODUCT OF 𝐉𝚰𝚴𝚮𝐘𝐔𝐊`",
-  author: process.env.PACK_AUTHER || "𝐉𝚰𝚴𝚮𝐘𝐔𝐊",
-  packname: process.env.PACK_NAME || "𝐉𝐈𝐍𝐇𝐘𝐔𝐊-𝐌𝐃",
+  author: process.env.PACK_AUTHER || "𝐬𝐨𝐥𝐢𝐭𝐚𝐢𝐫𝐞 𝐦𝐝",
+  packname: process.env.PACK_NAME || "𝐋𝐢𝐦𝐮𝐥 𝐬𝐚𝐬𝐚𝐤𝐢 -𝐌𝐃",
   botname: process.env.BOT_NAME || "𝐉𝚰𝚴𝚮𝐘𝐔𝐊-𝚳𝐃",
   ownername: process.env.OWNER_NAME || "𝑃𝒂𝒚𝒆𝒓 𝑈𝒏𝒌𝒏𝒐𝒘𝒏",
   errorChat: process.env.ERROR_CHAT || "",
